b8717340715e4e679f6a4e2ea575b2db2cfce54e
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-04 22:26:03 +0100

    add console.log and ||

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 
-function getTriangleArea(a, h){
-	if ( (a <= 0) && (h <= 0) ) {
+function getTriangleArea(a, h) {
+	if (a <= 0 || h <= 0) {
 		return 'Nieprawidłowe dane';
 	}
 	
-	if ( (a > 0) && (h > 0) ) {
+	if (a > 0 || h > 0) {
 		return 'a*h/2';
 	}
-}
+};
 console.log( getTriangleArea(10, 6) );
 
 var triangle1Area = getTriangleArea(10, 15),
 	triangle2Area = getTriangleArea(13, 8),
-	triangle3Area = getTriangleArea(2, 6);
+	triangle3Area = getTriangleArea(1, -1);
 
-
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
