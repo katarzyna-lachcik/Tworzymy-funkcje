798d91ffd7ea25377a1d5cf92de4217dc7ddb1cb
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-04 21:41:24 +0100

    my first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(13, 8),
+	triangle3Area = getTriangleArea(2, 6);
+
+function getTriangleArea(a, h){
+	if ( (a <= 0) && (h <= 0) ) {
+		return 'Nieprawidłowe dane';
+	}
+	
+	if ( (a > 0) && (h > 0) ) {
+		return 'a*h/2';
+	}
+}
+console.log( getTriangleArea(10, 6) )
