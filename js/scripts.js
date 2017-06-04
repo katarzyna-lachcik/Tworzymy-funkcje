77999aecafb24bb75ed95c9007ad079c9c458cd3
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-04 22:07:16 +0100

    small change

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-var triangle1Area = getTriangleArea(10, 15),
-	triangle2Area = getTriangleArea(13, 8),
-	triangle3Area = getTriangleArea(2, 6);
 
 function getTriangleArea(a, h){
 	if ( (a <= 0) && (h <= 0) ) {
@@ -11,4 +8,10 @@ function getTriangleArea(a, h){
 		return 'a*h/2';
 	}
 }
-console.log( getTriangleArea(10, 6) )
+console.log( getTriangleArea(10, 6) );
+
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(13, 8),
+	triangle3Area = getTriangleArea(2, 6);
+
+
