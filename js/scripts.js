b2be9a2fcf9dfa9b69bb4f871a831309f3a99aca
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-05 16:51:50 +0100

    change the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 
 function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0) {
+	if (a <= 0 || h <= 0){
 		return 'Nieprawidłowe dane';
 	}
-	
-	if (a > 0 || h > 0) {
+	else {
 		return 'a*h/2';
 	}
-};
+}
 console.log( getTriangleArea(10, 6) );
 
 var triangle1Area = getTriangleArea(10, 15),
